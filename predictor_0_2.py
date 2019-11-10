4b0a6e1df3e1bbf589b978d8e64fa5386fda40d0
Nofal Aamir <nofalaamir@gmail.com>
2019-11-09 22:48:04 -0800

    BTB is now a full dictionay, for easily updating BTB using kwargs

--- predictor_0_2.py
+++ predictor_0_2.py
@@ -3,14 +3,40 @@ import logging
 import argparse
 
 ####### I N I T #####################
-# the BTB will be a dictionary with index determined from last 3 words of PC
-# i.e. 400>1c4< -> 1*16^2 + 12+16 + 4 = 452 >> 2 (right-shift by 2 OR divide by 4)
-#     ..>0001 1100 01<00
 btb = dict()
+''' Design of BTB
+entry  :  PC TargetPC LocalPred Global[0,0] Global[0,1] Global[1,0] Global[1,1] Selector
+index     0     1         2        3           4            5            6          7    
+'''
 TAKEN = 1
 NOT_TAKEN = 0
 
+# btb = {
+#     entry :
+#     {
+#         "pc" : 0,
+#         "tpc" : 0,
+#         "local_pred" : 0,
+#         "g00" : 0,
+#         "g01" : 0,
+#         "g10" : 0,
+#         "g11" : 0,
+#         "sel" : 0
+#     }
+# }
+
 stats_local = {
+    "hits": 0,
+    "misses":	0,
+	"right_pred": 0,
+	"wrong_pred": 0,
+	"wrong_address": 0,
+	"collisions": 0
+}
+
+global_hist = [0,0]
+
+stats_global = {
     "hits": 0,
     "misses":	0,
 	"right_pred": 0,
@@ -23,11 +49,15 @@ logging.basicConfig(filename='cpts561_log.log', filemode='w', level=logging.DEBU
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--codefile", help="provide the file path to file containing opcodes")
+parser.add_argument("--type", help="local, global or tournament")
 args = parser.parse_args()
 
 ####### F U N C T I O N S #####################
 def get_entry_BTB(code):
     ''' assume input as hex'''
+    # the BTB will be a dictionary with index determined from last 3 words of PC
+    # i.e. 400>1c4< -> 1*16^2 + 12+16 + 4 = 452 >> 2 (right-shift by 2 OR divide by 4)
+    #     ..>0001 1100 01<00
     total = 0
     last_three = code[3:6]
     # reverse them so math is easier
@@ -43,9 +73,16 @@ def get_entry_BTB(code):
     logging.info("entry number for BTB: {}".format(entry))
     return entry
 
-def update_BTB(entry, pc, tpc, local_pred):
+def update_BTB(entry, **kwargs):
+    ''' this is where data is added to BTB '''
     # mod so we only have 1024 entries
-    btb[entry % 1024] = [pc, tpc, local_pred]
+    # btb[entry % 1024] = [pc, tpc, local_pred, g00, g01, g10, g11, sel]
+    temp_btb = dict()
+    for key, val in kwargs.items():
+        # print("key:{}, val:{}".format(key, val))
+        temp_btb[key] = val
+
+    btb[entry] = temp_btb
 
 def print_stats(stats):
     print("hits:{} -- misses:{} -- right predictions:{} -- wrong predictions:{}".format(
@@ -61,35 +98,43 @@ def print_BTB(sort):
         sorted_btb = sorted(btb)
         for entry in sorted_btb:
             print("{} --- PC {} - targetPC {} - local_pred {}".format(entry,
-                                                        btb[entry][0], 
-                                                        btb[entry][1],
-                                                        btb[entry][2]))
+                                                        btb[entry]["pc"], 
+                                                        btb[entry]["tpc"],
+                                                        btb[entry]["local_pred"]))
     elif sort == False:
         for entry in btb:
             print("{} --- PC {} - targetPC {} - local_pred {}".format(entry, 
-                                                        btb[entry][0], 
-                                                        btb[entry][1],
-                                                        btb[entry][2]))
+                                                        btb[entry]["pc"], 
+                                                        btb[entry]["tpc"],
+                                                        btb[entry]["local_pred"]))
 
 def in_BTB(entry, pc):
     if entry in btb:
         logging.debug("entry in BTB!")
-        if pc == btb[entry][0]:
+        if pc == btb[entry]["pc"]:
             logging.debug("PC found in BTB!")
             logging.debug("BTB entry {} --- PC {} - targetPC {} - local_pred {}".
                                                     format(entry, 
-                                                    btb[entry][0], 
-                                                    btb[entry][1],
-                                                    btb[entry][2]))
+                                                    btb[entry]["pc"], 
+                                                    btb[entry]["tpc"],
+                                                    btb[entry]["local_pred"]))
             return True
         else:
             logging.debug("PC {} not in BTB, compared to {}".format(
                                                             pc,
-                                                            btb[entry][0]))
+                                                            btb[entry]["pc"]))
             return False
     else:
         logging.debug("PC {} not in BTB".format(pc))
         return False
+
+def update_global_hist(t_nt):
+    # whatever index 0 had in not relevant anymore
+    # replace with previous state
+    global_hist[0] = global_hist[1]
+    global_hist[1] = t_nt
+
+    logging.debug("global history: {}".format(global_hist))
 
 def update_pred(prev_pred, t_nt):
     # implement 2-bit prdictor state-machine
@@ -159,14 +204,17 @@ for i in range(0, len(code)-2):
         if in_BTB(entry, hex_pc) == True:
             # we know that branch was not taken
             stats_local["wrong_pred"] += 1
-
+            # we didn't take the branch so update history
+            update_global_hist(NOT_TAKEN)
             # take the Target PC value from BTB instead of pc+4
-            # another solution may be to not updated the BTB! just update the prediction
-            # keep = btb[entry % 1024][1]
-            pred = update_pred(prev_pred=btb[entry][2], t_nt=NOT_TAKEN)
-            # update_BTB(entry, pc=hex_pc, tpc=keep, local_pred=pred)
+            # another solution is to not updated the BTB! just update the prediction
+            keep = btb[entry % 1024]["tpc"]
+            pred = update_pred(prev_pred=btb[entry]["local_pred"], t_nt=NOT_TAKEN)
+            update_BTB(entry, pc=hex_pc, tpc=keep, local_pred=pred)
     else:
         # FOUND a Branch hence TAKEN
+        # update global history
+        update_global_hist(TAKEN)
         logging.warning("FOUND a Branch for PC:{}".format(code[i]))
         
         # see if PC exists in current BTB
@@ -178,21 +226,26 @@ for i in range(0, len(code)-2):
             # THIS IS ALREADY BEING DONE as we update target PC every iteration
 
             # see if target PC in BTB is same as the next PC
-            if btb[entry][1] == hex_pc_plus1:
+            if btb[entry]["tpc"] == hex_pc_plus1:
                 # correct prediction!
                 stats_local["right_pred"] += 1
             else:
                 stats_local["wrong_pred"] += 1
 
-            # update prediction according to state machine
-            pred = update_pred(prev_pred=btb[entry][2], t_nt=TAKEN)
-            # we know the Target PC is the next instruction hence code[i+1]
-            update_BTB(entry, pc=hex_pc, tpc=hex_pc_plus1, local_pred=pred)
+            if args.type == "local":
+                # update prediction according to state machine
+                pred = update_pred(prev_pred=btb[entry]["local_pred"], t_nt=TAKEN)
+                # we know the Target PC is the next instruction hence code[i+1]
+                update_BTB(entry, pc=hex_pc, tpc=hex_pc_plus1, local_pred=pred)
+            # elif args.type == "global":
+            # elif args.type == "tournament":
+
         else:
-            # by default we'll assume Strong Taken
-            update_BTB(entry, pc=hex_pc, tpc=hex_pc_plus1, local_pred=[1,1])
-            # this is a TAKEN branch which was not in BTB hence miss
-            stats_local["misses"] += 1
+            if args.type == "local":
+                # by default we'll assume Strong Taken
+                update_BTB(entry, pc=hex_pc, tpc=hex_pc_plus1, local_pred=[1,1])
+                # this is a TAKEN branch which was not in BTB hence miss
+                stats_local["misses"] += 1
 
 # printing final state of BTB
 print_BTB(sort=True)
