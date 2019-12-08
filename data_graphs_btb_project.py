7b784b1fea6dee706fe0c7cdbf167d4a4c6e7028
Nofal Aamir <nofalaamir@gmail.com>
2019-12-07 19:16:38 -0800

    removed printBTB

--- data_graphs_btb_project.py
+++ data_graphs_btb_project.py
@@ -1,6 +1,6 @@
 import btb_project
 from btb_project import btb_main
-from btb_project import print_BTB
+from btb_project import print_stats
 from btb_project import stats
 
 abs_path_sample_trace = "C:\\Users\\nofal\\OneDrive\\Documents\\git\\cpts_561_BTB_project\\trace_sample.txt"
@@ -8,4 +8,4 @@ abs_path_Li_int = "C:\\Users\\nofal\\OneDrive\\Documents\\git\\cpts_561_BTB_proj
 abs_path_Spice_FP = "C:\\Users\\nofal\\OneDrive\\Documents\\git\\cpts_561_BTB_project\\benchmark\\Spice_FP.txt"
 
 btb_main("local", abs_path_sample_trace)
-print_BTB(stats)
+print_stats(stats)
